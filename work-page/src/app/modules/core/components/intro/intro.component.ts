6b11137eafb7e71d6ac9f7c680880fdee07bcdb9
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-02-12 01:11:41 -0300

    Rendering Buttons With Icons

--- work-page/src/app/modules/core/components/intro/intro.component.ts
+++ work-page/src/app/modules/core/components/intro/intro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionLogoLinkedin, ionLogoVercel, ionLogoInstagram } from '@ng-icons/ionicons'
 @Component({
   selector: 'work-page-intro',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.scss'
+  styleUrl: './intro.component.scss',
+  providers: provideIcons({ ionLogoLinkedin, ionLogoVercel, ionLogoInstagram })
 })
 export class IntroComponent {
 
