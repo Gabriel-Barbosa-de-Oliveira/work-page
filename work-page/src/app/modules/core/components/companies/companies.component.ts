c64ad2e34ab570bb4dd242b3f8dc2cc76893d51d
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-02-12 01:27:37 -0300

    Created other components structures

---
+++ work-page/src/app/modules/core/components/companies/companies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'work-page-companies',
+  imports: [],
+  templateUrl: './companies.component.html',
+  styleUrl: './companies.component.scss'
+})
+export class CompaniesComponent {
+
+}
