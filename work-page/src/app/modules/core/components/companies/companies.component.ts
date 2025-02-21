841f02e1881d4049aada9dd0101529cc72851c05
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-02-20 23:50:43 -0300

    Fix Work Experience Presentation

--- work-page/src/app/modules/core/components/companies/companies.component.ts
+++ work-page/src/app/modules/core/components/companies/companies.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { SectionTitleComponent } from "../section-title/section-title.component";
+import { CommonModule } from '@angular/common';
+import { Company } from '../../interfaces/company';
+import { Companies } from '../../constants/companies';
 
 @Component({
   selector: 'work-page-companies',
-  imports: [SectionTitleComponent],
+  imports: [SectionTitleComponent, CommonModule],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.scss'
 })
 export class CompaniesComponent {
+
+  public companies: Array<Company> = Companies;
 
 }
---
+++ work-page/src/app/modules/core/interfaces/company.ts
@@ -0,0 +1,9 @@
+export interface Company {
+    jobTitle: string,
+    companyName: string,
+    positionType: string,
+    initialDate: string,
+    finalDate: string,
+    workingTime: string,
+    jobDescription: string
+}
